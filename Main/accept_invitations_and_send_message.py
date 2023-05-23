ce8b825500756f975732445d0713fbc7a1c85c42
Amey <bamey2241997@gmail.com>
2023-05-22 22:29:18 -0700

    Added Changnes

---
+++ Main/accept_invitations_and_send_message.py
@@ -0,0 +1,10 @@
+from login import Login
+from Configs.messageConfigs import accept_invite_message_body
+from Configs.jobConfigs import *
+
+
+def main():
+    invitations_recevied = api.get_invitations(limit=1)
+    accept_invite_message_body
+    api = Login()
+
