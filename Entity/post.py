bd22001c092eb6556016d60b53645581399fe48c
Amey <bamey2241997@gmail.com>
2023-05-23 16:51:06 -0700

    Added Entities

--- Entity/post.py
+++ Entity/post.py
@@ -1,6 +1,6 @@
 class posts:
     def __init__(self):
-        post_authour_name = None
-        post_authour_profile = None
-        post_content = None
-        post_url = None
+        self.post_authour_name = None
+        self.post_authour_profile = None
+        self.post_content = None
+        self.post_url = None
--- Entity/urnPrefixes.py
+++ Entity/urnPrefixes.py
@@ -1,3 +1,19 @@
 class Prefix:
-    def __init__():
-        geoUrn = ""
+    def __init__(self):
+        self.constant_prefix="urn:li:"
+        self.geoUrn = "fs_geo:"
+        self.industryUrn = "fs_industry:"
+        self.profileEntityUrn = "fs_profile:"
+        self.dashedProfileEntityUrn = "fsd_profile:"
+        self.miniProfileUrn = "fs_miniProfile:"
+        self.memberUrn = "member:"
+        self.companyUrn = "fs_miniCompany:"
+        self.schoolObjectUrn = "school:"
+        self.schoolEntityUrn = "fs_miniSchool:"
+        self.dashedCompanyUrn = "fsd_company:"
+        self.invitationRelUrn = "fs_relInvitation:"
+        self.invitationMailBoxItemUrn = "invitation:"
+        self.dashedInvitationUrn = "fsd_invitation:"
+
+
+
